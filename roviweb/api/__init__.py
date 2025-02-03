488538a678b16b433b460f1f278291ec50c61380
Logan Ward <ward.logan.t@gmail.com>
2025-02-03 17:15:41 -0500

    Add a status page for a particular cell

--- roviweb/api/__init__.py
+++ roviweb/api/__init__.py
@@ -2,11 +2,13 @@
 from pathlib import Path
 import logging
 
+import numpy as np
+import pandas as pd
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.templating import Jinja2Templates
 
-from . import db, online
+from . import db, online, state
 
 logger = logging.getLogger(__name__)
 
@@ -26,4 +28,22 @@ templates = Jinja2Templates(directory=Path(__file__).parent / "templates")
 async def home(request: Request):
     return templates.TemplateResponse(
         request=request, name="home.html"
+    )
+
+@app.get("/{name}")
+async def status(request: Request, name: str):
+    # Get the estimator status, if available
+    table = None
+    print(state.estimators)
+    if name in state.estimators:
+        est = state.estimators[name]
+        est_state = est.estimator.state
+        table = pd.DataFrame({
+            'name': est.estimator.state_names,
+            'value': est_state.get_mean(),
+            'std': np.sqrt(np.diag(est_state.get_covariance()))
+        })
+
+    return templates.TemplateResponse(
+        request=request, name="status.html", context={'name': name, 'table': table}
     )
